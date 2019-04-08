ee1aa7575595d09b31469f8543613118211c6fd1
Anthony <anthony.marque@gmail.com>
2019-04-08 15:56:45 +0200

    add it all baby

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ChatService, Message } from "../chat.service";
+import "rxjs/add/operator/scan";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'chat-dialog',
-  templateUrl: './chat-dialog.component.html',
-  styleUrls: ['./chat-dialog.component.css']
+  selector: "chat-dialog",
+  templateUrl: "./chat-dialog.component.html",
+  styleUrls: ["./chat-dialog.component.css"]
 })
 export class ChatDialogComponent implements OnInit {
+  messages: Observable<Message[]>;
+  formValue: string;
 
-  constructor() { }
+  constructor(public chat: ChatService) {}
 
   ngOnInit() {
+    this.messages = this.chat.conversation
+      .asObservable()
+      .scan((acc, val) => acc.concat(val)
+      );
   }
 
+  sendMessage() {
+    this.chat.converse(this.formValue);
+    this.formValue = "";
+  }
 }
