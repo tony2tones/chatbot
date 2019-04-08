c473028dd6e5b64def9a543ce462b1934cc2fd94
Anthony <anthonymarques@outlook.com>
2019-04-08 20:44:08 +0200

    added rxjs-compat and updated tsconfig to declare the api.ai stream

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { ChatDialogComponent } from "./chat-dialog/chat-dialog.component";
 import { ChatService } from "./chat.service";
-
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
