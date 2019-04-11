81fedec00e05441302bf582ba4df0443230c4fa2
Anthony <anthony.marque@gmail.com>
2019-04-11 18:42:11 +0200

    adding validation for chatbot

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,37 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { ChatService, Message } from '../chat.service';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/scan'; 
-import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import {  } from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { ChatService, Message } from "../chat.service";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/scan";
+import {} from "@angular/forms";
 
 @Component({
-  selector: 'chat-dialog',
-  templateUrl: './chat-dialog.component.html',
-  styleUrls: ['./chat-dialog.component.css']
+  selector: "chat-dialog",
+  templateUrl: "./chat-dialog.component.html",
+  styleUrls: ["./chat-dialog.component.css"]
 })
 export class ChatDialogComponent implements OnInit {
-  title = 'Angular Bot';
+  title = "Angular Bot";
   messages: Observable<Message[]>;
   formValue: string;
 
-  constructor(public chat: ChatService,private fb: FormBuilder) { }
+  constructor(public chat: ChatService) {}
 
   ngOnInit() {
-    this.messages = this.chat.conversation.asObservable()
+    this.messages = this.chat.conversation
+      .asObservable()
       .scan((acc, val) => acc.concat(val));
   }
 
   sendMessage() {
-    this.chat.converse(this.formValue);
-    this.formValue = '';
-  }
-
-  form = this.fb.group ({
-     messageInput: ['', Validators.required]
-  });
-
-  get messageInput() {
-    return this.form.get('messageInput');
+    let value = this.formValue;
+    if (value === undefined || value === '') {
+      console.log('this is blooody empty ');
+    } else {   
+      this.chat.converse(value);
+      this.formValue = "";
+    }
   }
 }
